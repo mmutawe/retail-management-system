d88d237634a1102d82ecab135237c9cce08e0f62
mmutawe <mahmoudmutawe@gmail.com>
2023-07-02 15:44:13 -0500

    add resilience4j to order service; config circuitbreaker, timelimiter and retry

--- order-service/src/main/java/com/mmutawe/projects/ms/retail/order/controllers/OrderController.java
+++ order-service/src/main/java/com/mmutawe/projects/ms/retail/order/controllers/OrderController.java
@@ -3,14 +3,20 @@ package com.mmutawe.projects.ms.retail.order.controllers;
 import com.mmutawe.projects.ms.retail.order.dtos.OrderRequestDto;
 import com.mmutawe.projects.ms.retail.order.dtos.OrderResponseDto;
 import com.mmutawe.projects.ms.retail.order.services.OrderService;
+import com.mmutawe.projects.ms.retail.order.utils.Constant;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-import static com.mmutawe.projects.ms.retail.order.utils.Constant.ORDER_URI;
+import static com.mmutawe.projects.ms.retail.order.utils.Constant.*;
 
 @RestController
 @RequestMapping(ORDER_URI)
@@ -20,10 +26,23 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderResponseDto> placeOrder(@RequestBody OrderRequestDto orderRequestDto){
+    @Retry(name = RESILIENCE4J_INSTANCE_INVENTORY)
+    @TimeLimiter(name = RESILIENCE4J_INSTANCE_INVENTORY)
+    @CircuitBreaker(
+            name = "inventory",
+            fallbackMethod = INVENTORY_FALLBACK_METHOD_NAME)
+    public Mono<ResponseEntity<OrderResponseDto>> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
         OrderResponseDto orderResponseDto = orderService.placingOrder(orderRequestDto);
 
-        return ResponseEntity
-                .ok(orderResponseDto);
+        return Mono.fromSupplier(() -> ResponseEntity.ok(orderResponseDto));
     }
+
+    private Mono<ResponseEntity<Object>> inventoryServiceUnresponsiveFallBack(
+            OrderRequestDto orderRequestDto,
+            RuntimeException runtimeException) {
+        return Mono.fromSupplier(() -> ResponseEntity
+                        .status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("inventory-api is unable to handle order-api requests!"));
+    }
+
 }
--- order-service/src/main/java/com/mmutawe/projects/ms/retail/order/utils/Constant.java
+++ order-service/src/main/java/com/mmutawe/projects/ms/retail/order/utils/Constant.java
@@ -3,4 +3,8 @@ package com.mmutawe.projects.ms.retail.order.utils;
 public class Constant {
     public static final String ORDER_URI="/api/v1/order";
     public static final String SLASH="/";
+
+
+    public static final String INVENTORY_FALLBACK_METHOD_NAME="inventoryServiceUnresponsiveFallBack";
+    public static final String RESILIENCE4J_INSTANCE_INVENTORY="inventory";
 }
